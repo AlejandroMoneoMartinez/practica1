e6dcacb2b35ec50e9e30e7f5c81a0bc4cb19c259
Omar Ben <omaxlive3@gmail.com>
2020-03-29 15:23:53 +0200

    Create Tasks service

---
+++ src/main/java/com/example/practica1/service/TaskService.java
@@ -0,0 +1,37 @@
+package com.example.practica1.service;
+
+import com.example.practica1.entity.Task;
+import com.example.practica1.exceptions.TaskNotFoundException;
+import com.example.practica1.repository.TaskRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> listTasks() {
+        return (List<Task>) taskRepository.findAll();
+    }
+    
+    public Task getTaskById(long id) {
+
+        Optional<Task> optionalTask = taskRepository.findById(id);
+
+        if(optionalTask.isPresent())
+          return optionalTask.get();
+        else
+          throw new TaskNotFoundException("Task Not Found");
+
+    }
+
+}
