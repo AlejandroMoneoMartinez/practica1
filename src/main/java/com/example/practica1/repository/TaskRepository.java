51de9be18a92406a1b97d44b1d6c41656b975fdc
Omar Ben <omaxlive3@gmail.com>
2020-03-29 15:26:37 +0200

    Override CrudRepository methods in Task repository

--- src/main/java/com/example/practica1/repository/TaskRepository.java
+++ src/main/java/com/example/practica1/repository/TaskRepository.java
@@ -1,9 +1,15 @@
 package com.example.practica1.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.example.practica1.entity.Task;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TaskRepository extends CrudRepository<Task, Long> {
+    @Override
+    List<Task> findAll();
+    Optional<Task> findById(String name);
 }
